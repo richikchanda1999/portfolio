a76c2b07ccf97263495f4bed99a9204141b5586a
Richik Chanda <richikchanda1999@gmail.com>
2023-09-26 11:05:25 +0530

    fix: adapt to mobile UI

--- src/app/Sections/Intro.tsx
+++ src/app/Sections/Intro.tsx
@@ -10,14 +10,14 @@ export default function Section1() {
       <Image src="/scribbles.png" alt="heading" fill />
 
       <div className="flex flex-col items-center w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col justify-center text-6xl">
+        <div className="flex flex-col justify-center text-3xl sm:text-5xl md:text-6xl">
           <h1 className={`${fascinateInline.className} text-[#BF1363]`}>
             Build what matters,
           </h1>
           <h1 className="mt-4 italic text-right bold">One block at a time.</h1>
         </div>
 
-        <div className="flex flex-col justify-start items-center text-2xl mt-16 max-h-96 overflow-auto gap-1">
+        <div className="flex flex-col justify-start items-center text-xl md:text-2xl mt-16 max-h-96 overflow-auto gap-1">
           <h1>
             Hey, I&apos;m{" "}
             <span className="italic underline underline-offset-2 decoration-wavy text-3xl">
